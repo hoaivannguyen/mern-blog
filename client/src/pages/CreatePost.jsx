3b89c5139d15c36ba21b96f4cf5426a7877a769d
hoaivannguyen <nguyenvohoaivan@gmail.com>
2024-07-29 22:36:45 +0700

    complete create post functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -3,16 +3,19 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { getStorage, ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 import { app } from "../firebase";
-import { set } from "mongoose";
 import { useState } from "react";
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import { useNavigate } from "react-router-dom";
 
 export default function CreatePost() {
   const [file, setFile] = useState([]);
   const [imageUploadProgress, setImageUploadProgress] = useState(null);
   const [imageUploadError, setImageUploadError] = useState(null);
   const [formData, setFormData] = useState({});
+  const [publishError, setPublishError] = useState(null);
+  const navigate = useNavigate();
+
   const handleUploadImage = async () => {
     try {
       if (!file) {
@@ -49,66 +52,97 @@ export default function CreatePost() {
     }
   }
 
-  return <div className="p-3 max-w-3xl mx-auto min-h-screen">
-    <h1 className="text-3xl text-center my-7 font-semibold">Create a post</h1>
-    <form className="flex flex-col gap-4">
-      <div className="flex flex-col gap-4 sm:flex-row justify-between">
-        <TextInput 
-          type="text" 
-          id="title" 
-          placeholder="Title" 
-          required 
-          className="flex-1" 
-        />
-        <Select>
-          <option value={"uncategorized"}>Select a category</option>
-          <option value={"javscript"}>Javascript</option>
-          <option value={"reactjs"}>React.js</option>
-          <option value={"nextjs"}>Next.js</option>
-        </Select>
-      </div>
-      <div className="flex gap-4 items-center justify-between border-4
-      border-teal-500 border-dotted p-3">
-        <FileInput 
-          type="file" 
-          accept="image/*" 
-          onChange={(e)=>setFile(e.target.files[0])}
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("/api/post/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        setPublishError(data.message);
+        return
+      }
+      if (res.ok) {
+        setPublishError(null);
+        navigate(`/post/${data.slug}`);
+      } 
+    } catch (error) {
+      setPublishError("Something went wrong");
+    }
+  }
+
+  return (
+    <div className="p-3 max-w-3xl mx-auto min-h-screen">
+      <h1 className="text-3xl text-center my-7 font-semibold">Create a post</h1>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-4 sm:flex-row justify-between">
+          <TextInput 
+            type="text" 
+            id="title" 
+            placeholder="Title" 
+            required 
+            className="flex-1"
+            onChange={(e) => setFormData({ ...formData, title: e.target.value })} 
           />
-        <Button 
-          type="button" 
-          gradientDuoTone={"purpleToBlue"} 
-          size={"sm"} 
-          outline
-          onClick={handleUploadImage}
-          disabled={imageUploadProgress}
-        >
-          {imageUploadProgress ? (
-            <div className="w-16 h-16">
-              <CircularProgressbar 
-                value={imageUploadProgress} 
-                text={`${imageUploadProgress || 0}%`} 
-              />
-            </div>
-          ) : (
-              "Upload Image"
-          )}
+          <Select
+            onChange={(e) => setFormData({ ...formData, title: e.target.value })}  
+          >
+            <option value={"uncategorized"}>Select a category</option>
+            <option value={"javscript"}>Javascript</option>
+            <option value={"reactjs"}>React.js</option>
+            <option value={"nextjs"}>Next.js</option>
+          </Select>
+        </div>
+        <div className="flex gap-4 items-center justify-between border-4
+        border-teal-500 border-dotted p-3">
+          <FileInput 
+            type="file" 
+            accept="image/*" 
+            onChange={(e)=>setFile(e.target.files[0])}
+            />
+          <Button 
+            type="button" 
+            gradientDuoTone={"purpleToBlue"} 
+            size={"sm"} 
+            outline
+            onClick={handleUploadImage}
+            disabled={imageUploadProgress}
+          >
+            {imageUploadProgress ? (
+              <div className="w-16 h-16">
+                <CircularProgressbar 
+                  value={imageUploadProgress} 
+                  text={`${imageUploadProgress || 0}%`} 
+                />
+              </div>
+            ) : (
+                "Upload Image"
+            )}
+          </Button>
+        </div>
+        {imageUploadError && <Alert color="failure">{imageUploadError}</Alert>}
+        {formData.image && 
+          <img 
+            className="w-full h-72 object-cover" 
+            src={formData.image} 
+            alt="upload"/>}
+        <ReactQuill 
+          theme="snow" 
+          placeholder="Write something..." 
+          className="h-72 mb-12"
+          required
+          onChange={(value) => setFormData({ ...formData, content: value })}
+        />
+        <Button type="submit" gradientDuoTone={"purpleToBlue"}>
+          Publish 
         </Button>
-       </div>
-      {imageUploadError && <Alert color="failure">{imageUploadError}</Alert>}
-      {formData.image && 
-        <img 
-          className="w-full h-72 object-cover" 
-          src={formData.image} 
-          alt="upload"/>}
-      <ReactQuill 
-        theme="snow" 
-        placeholder="Write something..." 
-        className="h-72 mb-12"
-        required
-      />
-      <Button type="submit" gradientDuoTone={"purpleToBlue"}>
-        Publish
-      </Button>
-    </form>
+        {publishError && <Alert className="mt-5" color="failure">{publishError}</Alert>}
+      </form>
   </div>
+  )
 }
